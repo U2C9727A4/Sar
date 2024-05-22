5500825fa5925c50bfffddb1c4a3d6a7a727463f
U2C9727A4 <naciyealci0@gmail.com>
2024-05-22 08:30:12 +0300

    New function read_file_perms()

--- sar.c
+++ sar.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <sys/stat.h>
 
 /* Archive Stucture
 
@@ -44,33 +45,51 @@ int get_blocks(FILE *input_file, size_t start_at, uint64_t uint64_array[4])
     // This function reads 4 uint64_t's from the input_file at start_at
     // and saves it in uint64_array as described above.
     // Restores file seek number, thus dosen't change the inital value of it.
+    // Returns -1 if it hits EOF on the archive.
 
     uint64_t before_seek = ftell(input_file);
     fseek(input_file, start_at, SEEK_SET);
-    fread(uint64_array, sizeof(uint64_t), 4, input_file);
+    if ((fread(uint64_array, sizeof(uint64_t), 4, input_file)) == 0)
+    {
+      return -1;
+    }
     fseek(input_file, before_seek, SEEK_SET);
 
     return 0;
 }
 
-char* read_metadata(uint64_t blocks[4], FILE* input_file)
+char** read_metadata(uint64_t blocks[4], FILE* input_file)
 {
   /*
     WARNING! This function returns a heap-allocated string. If this function fails, it returns NULL and frees output.
     If it dosen't fail, the string it returns must be freed by the scope calling the function.
-    This function reads metadata from the archive, returns a char* (Which is the entire metadata and split() must be used to make sense of it.)*/
+    This function reads metadata from the archive, returns a char* (Which is the entire metadata)
+     And split() must be used to make sense of the data.
+    */
   uint64_t oldSeek = ftell(input_file);
   fseek(input_file, blocks[0], SEEK_SET);
   char* output = malloc(blocks[1] - blocks[0]);
-   if ((fread(output, (blocks[1] - blocks[0]), 1, input_file)) != 0)
-   {
+  if ((fread(output, (blocks[1] - blocks[0]), 1, input_file)) != 0)
+  {
     free(output);
     fseek(input_file, oldSeek, SEEK_SET);
     return NULL;
-   }
+  }
   fseek(input_file, oldSeek, SEEK_SET);
   return output;
 
+}
+char* read_file_perms(const char* file_path)
+{
+  /*
+  This function reads file permission, and returns its numeric representation as a char*.
+  Note: This function is designed to be directly writeable to archive metadata, and thus return is newline terminated.
+  */
+ struct stat file_stat;
+ stat(file_path, &file_stat);
+ char* permissions;
+ sprintf(permissions, "%o\n", file_stat.st_mode & 0777);
+ return permissions;
 }
 
 
@@ -79,7 +98,7 @@ char** split(char *input, short* element_ptr, const char* seperator)
 {
   /*
     This function takes in a string, splits it based on seperator and returns as a string array (char**).
-  
+    !! The inputted string must be null terminated.
   */
   short iterations = 0;
   short elements = 0;
